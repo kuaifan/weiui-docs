79cbc295cb68680fb48723bba55b24cad4647e2e
kuaifan <aipaw@live.cn>
2019-04-01 18:43:23 +0800

    no message

---
+++ .vuepress/dist/assets/js/15.27f717f7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{188:function(t,n,s){},267:function(t,n,s){"use strict";var i=s(188);s.n(i).a},381:function(t,n,s){"use strict";s.r(n);s(267);var i=s(2),e=Object(i.a)({},function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"md-badge-ios"},[this._v("仅 iOS")])},[],!1,null,null,null);n.default=e.exports}}]);
--- .vuepress/theme/layouts/404.vue
+++ .vuepress/theme/layouts/404.vue
@@ -1,28 +1,36 @@
 <template>
-  <div class="theme-container">
-    <div class="content">
-      <h1>404</h1>
-      <blockquote>{{ getMsg() }}</blockquote>
-      <router-link to="/">Take me home.</router-link>
+    <div class="theme-container">
+        <div class="content">
+            <h1>404</h1>
+            <blockquote>{{ getMsg() }}</blockquote>
+            <router-link to="/">Take me home.</router-link>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-const msgs = [
-  `There's nothing here.`,
-  `How did we get here?`,
-  `That's a Four-Oh-Four.`,
-  `Looks like we've got some broken links.`
-]
+    import redirect from './redirect'
 
-export default {
-  methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
+    const msgs = [
+        `There's nothing here.`,
+        `How did we get here?`,
+        `That's a Four-Oh-Four.`,
+        `Looks like we've got some broken links.`
+    ];
+
+    export default {
+        created() {
+            let newUrl = redirect[this.$route.path];
+            if (typeof newUrl === "string" && newUrl) {
+                this.$router.push(newUrl);
+            }
+        },
+        methods: {
+            getMsg() {
+                return msgs[Math.floor(Math.random() * msgs.length)]
+            },
+        }
     }
-  }
-}
 </script>
 
 <style src="../styles/theme.styl" lang="stylus"></style>
